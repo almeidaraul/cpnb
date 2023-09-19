10b99cc2cd0ae1208c2ef86d9f2f03d57a47b7d9
Raul Almeida <almeidaraulgp@gmail.com>
2023-09-19 14:44:26 -0300

    upd template with pbds

--- code/extra/template.cpp
+++ code/extra/template.cpp
@@ -7,6 +7,13 @@ using ll = long long;
 using ii = pair<int, int>;
 using wv = pair<ll, int>;
 
+// PBDS ----
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+// ---------
+
 signed main() {
   cin.tie(0)->sync_with_stdio(0);
 }
